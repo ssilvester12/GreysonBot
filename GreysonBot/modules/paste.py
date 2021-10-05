b2a7c5b400417b0cb4648f2a40102c67e4439714
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-10-05 23:07:37 +0530

    Update paste.py

--- GreysonBot/modules/paste.py
+++ GreysonBot/modules/paste.py
@@ -1,6 +1,6 @@
 import requests
-from Hackfreaks import dispatcher
-from Hackfreaks.modules.disable import DisableAbleCommandHandler
+from GreysonBot import dispatcher
+from GreysonBot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
