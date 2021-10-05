205ae8fc3d7cc02d47a74d7c439ccf2574436200
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-10-05 23:03:23 +0530

    Update modules.py

--- GreysonBot/modules/modules.py
+++ GreysonBot/modules/modules.py
@@ -1,10 +1,10 @@
 import importlib
 
-from Hackfreaks import dispatcher
-from Hackfreaks.__main__ import (CHAT_SETTINGS, DATA_EXPORT, DATA_IMPORT,
+from GreysonBot import dispatcher
+from GreysonBot.__main__ import (CHAT_SETTINGS, DATA_EXPORT, DATA_IMPORT,
                                    HELPABLE, IMPORTED, MIGRATEABLE, STATS,
                                    USER_INFO, USER_SETTINGS)
-from Hackfreaks.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from GreysonBot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
