7a3e38c2c067ede07bc42d2b281a5e362684b1f3
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-10-05 22:36:01 +0530

    Update debug.py

--- GreysonBot/modules/debug.py
+++ GreysonBot/modules/debug.py
@@ -5,8 +5,8 @@ from telethon import events
 from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
-from Hackfreaks import telethn, dispatcher
-from Hackfreaks.modules.helper_funcs.chat_status import dev_plus
+from GreysonBot import telethn, dispatcher
+from GreysonBot.modules.helper_funcs.chat_status import dev_plus
 
 DEBUG_MODE = False
 
