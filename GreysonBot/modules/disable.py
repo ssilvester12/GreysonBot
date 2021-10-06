3c23ec68baf2b59f48a08d174dc9cf456144d93b
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-10-06 23:29:28 +0530

    Import chat status

--- GreysonBot/modules/disable.py
+++ GreysonBot/modules/disable.py
@@ -15,9 +15,9 @@ FILENAME = __name__.rsplit(".", 1)[-1]
 # If module is due to be loaded, then setup all the magical handlers
 if is_module_loaded(FILENAME):
 
-    from Hackfreaks.modules.helper_funcs.chat_status import (
+    from GreysonBot.modules.helper_funcs.chat_status import (
         connection_status, is_user_admin, user_admin)
-    from Hackfreaks.modules.sql import disable_sql as sql
+    from GreysonBot.modules.sql import disable_sql as sql
     from telegram.ext.dispatcher import run_async
 
     DISABLE_CMDS = []
