59b5622282c358eabe2fcaaa7d3912bf0d183b1a
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-10-06 23:42:27 +0530

    Update quote.py

--- GreysonBot/modules/quote.py
+++ GreysonBot/modules/quote.py
@@ -12,7 +12,7 @@ import random
 import json
 import os
 import re
-from GreysonBot.GreysonBot import greyson
+from GreysonBot import greyson
 
 COLORS = [
     "#F07975", "#F49F69", "#F9C84A", "#8CC56E", "#6CC7DC", "#80C1FA", "#BCB3F9", "#E181AC"]
