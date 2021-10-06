01e19e4c7046fdc352d099ffa13d4cb2ec1e1834
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-10-06 23:44:37 +0530

    Imported greyson successfully

--- GreysonBot/modules/telebans.py
+++ GreysonBot/modules/telebans.py
@@ -1,4 +1,4 @@
-from GreysonBot.GreysonBot import GreysonBot
+from GreysonBot import greyson
 from GreysonBot import telethn
 from telethon import events
 from GreysonBot.modules.helper_funcs.telethn.chatstatus import (
