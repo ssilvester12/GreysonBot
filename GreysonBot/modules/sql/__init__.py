9d6107bbb8f7f10e4280f0367e30ff0a6ff1b604
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-10-06 22:29:32 +0530

    Update __init__.py

--- GreysonBot/modules/sql/__init__.py
+++ GreysonBot/modules/sql/__init__.py
@@ -1,8 +1,7 @@
+from GreysonBot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
-
-from GreysonBot import DB_URI
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 def start() -> scoped_session:
